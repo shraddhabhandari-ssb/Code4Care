50d4ab885f28e90e6ea0f0e92fb301a4c643d593
shraddhabhandari-ssb <shraddhab591@gmail.com>
2026-08-10 15:57:20 +0530

    NEW CODE

---
+++ mannmitra/src/main/java/com/core2web/Main.java
@@ -0,0 +1,8 @@
+package com.core2web;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("New Project Created");
+    }
+}
