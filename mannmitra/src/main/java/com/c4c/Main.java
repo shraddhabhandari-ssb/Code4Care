8a369defaaddc13b8209ce6fff5fa7e23a9110fa
Amol Shinde <amolshinde252006@gmail.com>
2026-08-12 15:31:34 +0530

    New project added

--- mannmitra/src/main/java/com/c4c/Main.java
+++ mannmitra/src/main/java/com/c4c/Main.java
@@ -1,8 +1,7 @@
-package com.core2web;
+package com.c4c;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println("New Project Created");
     }
 }
