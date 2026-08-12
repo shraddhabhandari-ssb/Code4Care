fae1ae8b53af4ba631c9c3bfb3d74a32c6b05007
Amol Shinde <amolshinde252006@gmail.com>
2026-08-12 15:35:33 +0530

    Delete mannmitra directory

--- mannmitra/src/main/java/com/core2web/Main.java
+++
@@ -1,8 +0,0 @@
-package com.core2web;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println("New Project Created");
-    }
-}
